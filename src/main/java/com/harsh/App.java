ff002d441a3a7323b7546f3f4173e18e3b8388ee
Harsh Raghuwanshi <119292911+harsh201004@users.noreply.github.com>
2025-11-07 12:41:25 +0530

    Add webhook check message to main method
    Added a new print statement to check webhook functionality.

--- src/main/java/com/harsh/App.java
+++ src/main/java/com/harsh/App.java
@@ -4,6 +4,8 @@ public class App {
     
 public static void main(String[] args) {
         System.out.println("Hello from Harsh's Java App!");
+        System.out.println("Checking the pipeline... Webhook working");
     }
 }
+
 
