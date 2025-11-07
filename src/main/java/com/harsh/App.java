b8b348f14aff11cabb3177f4b7f8e2eac5b11815
Harsh Raghuwanshi <119292911+harsh201004@users.noreply.github.com>
2025-11-07 15:04:54 +0530

    Simple commit
    Added a new print statement to indicate the app is running.

--- src/main/java/com/harsh/App.java
+++ src/main/java/com/harsh/App.java
@@ -7,8 +7,10 @@ public static void main(String[] args) {
         System.out.println("Checking the pipeline... Webhook working");
         System.out.println("Everything is working");
         System.out.println("Checking...");
+        System.out.println("Checked... Running");
     }
 }
+
 
 
 
