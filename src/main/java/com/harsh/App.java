bb70710079ea5048d13e35ec79badac153598662
Harsh Raghuwanshi <119292911+harsh201004@users.noreply.github.com>
2025-11-07 14:36:58 +0530

    New commint for successful run
    Added a new print statement to indicate everything is working.

--- src/main/java/com/harsh/App.java
+++ src/main/java/com/harsh/App.java
@@ -5,7 +5,9 @@ public class App {
 public static void main(String[] args) {
         System.out.println("Hello from Harsh's Java App!");
         System.out.println("Checking the pipeline... Webhook working");
+        System.out.println("Everything is working");
     }
 }
+
 
 
